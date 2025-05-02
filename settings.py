f66155af6b35987c3162b45181fed69ce43e5beb
syed-afrid <afrid0052@gmail.com>
2025-05-02 22:56:28 +0530

    Settings module

---
+++ settings.py
@@ -0,0 +1,7 @@
+class Setting:
+    """A class to store all settings of Alien Invasions."""
+    def __init__(self):
+        """Initiate game settings."""
+        self.screen_width = 800
+        self.screen_height = 600
+        self.bg_color = (230,230,230)
