055d74e3145cab3ed34ea8affb9c6d567b47208a
syed-afrid <afrid0052@gmail.com>
2025-05-04 22:20:59 +0530

    ship added

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,17 +1,21 @@
 import sys
 import pygame
 from settings import Setting
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
+        # Screen
         self.settings = Setting()
         self.screen = pygame.display.set_mode((
             self.settings.screen_width,self.settings.screen_height
         ))
         pygame.display.set_caption("Alien Invasion")
+        # Ship
+        self.ship = Ship(self)
     def run_game(self):
         """Start the main loop for the game."""
         while True:
@@ -21,6 +25,7 @@ class AlienInvasion:
                     sys.exit()
             # Redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
             # Make the most recently drawn screen visible.
             pygame.display.flip()
 
--- settings.py
+++ settings.py
@@ -2,6 +2,6 @@ class Setting:
     """A class to store all settings of Alien Invasions."""
     def __init__(self):
         """Initiate game settings."""
-        self.screen_width = 800
-        self.screen_height = 600
+        self.screen_width = 1200
+        self.screen_height = 700
         self.bg_color = (230,230,230)
